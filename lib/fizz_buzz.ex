3f63c26253bc1dc5d43438d5fcf01e325a1673eb
JoaoFigueiro <joao.l.comiran@gmail.com>
2025-01-13 08:59:18 -0300

    feat: fizzbuzz

---
+++ lib/fizz_buzz.ex
@@ -0,0 +1,25 @@
+defmodule FizzBuzz do
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(", ")
+    |> Enum.map(&convert_and_evaluate_numbers/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #(reason)"
+
+  def convert_and_evaluate_numbers(elem) do
+    number = String.to_integer(elem)
+    evaluate_numbers(number)
+  end
+
+  def evalute_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evalute_numbers(number) when rem(number, 3) == 0, do: :fizz
+  def evalute_numbers(number) when rem(number, 5) == 0, do: :buzz
+  def evalute_numbers(number), do: number
+end
